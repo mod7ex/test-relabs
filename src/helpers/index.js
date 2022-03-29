32c4cd48ab1d80b9561dd5eddc2019f7808ef00e
Mourad EL CADI <mouradface1998@gmail.com>
2022-03-29 12:10:38 +0000

    fixed dates issues

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -8,12 +8,22 @@ export const debounce = function (func, timeout = 2000) {
       };
 };
 
+let concat_0 = (num) => {
+      if (`${num}`.length != 1) return num;
+
+      return "0" + num;
+};
+
 export const formatDateTime = (num) => {
-      let d = new Date(num);
+      let d = new Date(num * 1000);
 
       return (
-            [d.getMonth() + 1, d.getDate(), d.getFullYear()].join(".") +
+            [
+                  concat_0(d.getDate()),
+                  concat_0(d.getMonth() + 1),
+                  d.getFullYear(),
+            ].join(".") +
             " " +
-            [d.getHours(), d.getMinutes()].join(":")
+            [concat_0(d.getHours()), concat_0(d.getMinutes())].join(":")
       );
 };
